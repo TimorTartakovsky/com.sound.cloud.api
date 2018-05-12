a29c67d6c3833ec5529deec1b38db8a6356446a7
Timor Tartakovsky <timortartakovsky@gmail.com>
2018-05-12 23:16:35 +0300

    Project creation commit: Teymur Tartakovsky

---
+++ src/main/java/com/sound/cloud/api/soundTracker/SoundTrackerApplication.java
@@ -0,0 +1,12 @@
+package com.sound.cloud.api.soundTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoundTrackerApplication.class, args);
+	}
+}
---
+++ src/test/java/com/sound/cloud/api/soundTracker/SoundTrackerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sound.cloud.api.soundTracker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SoundTrackerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
